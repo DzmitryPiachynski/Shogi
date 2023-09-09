fb720b4a8896b27d3e8ddeb349ed5cb897978a7d
Dzmitry Piachynski <pechinskiy.dima@gmail.com>
2023-09-09 09:04:17 +0200

    Links page layout v2

--- src/pages/Links/Links.js
+++ src/pages/Links/Links.js
@@ -5,17 +5,26 @@ function Links() {
     return (
         <div className="holder">
             <h1>Useful links</h1>
+            <h2 className="pLinks">Shogi news</h2>
             <ul>
                 <li>
-                    <a href="https://example.com">Example Website</a>: A website for examples.
+                    <a href="http://shogihub.com/">ShogiHub</a> : News about the professional shogi world in English.
                 </li>
                 <li>
-                    <a href="https://google.com">Google</a>: The search engine giant.
+                    <a href="https://fesashogi.eu/">FESA</a> : Official website of the Federation of European Shogi Associations.
                 </li>
                 <li>
-                    <a href="https://github.com">GitHub</a>: The world's leading software development platform.
+                    <a href="http://shogi.pl/">Shogi Harbour</a> : An English-language website dedicated to Shogi promotion, including some tutorials.
                 </li>
-                {/* Добавьте еще ссылок с описаниями по вашему выбору */}
+            </ul>
+            <h2 className="pLinks">YouTube channels</h2>
+            <ul>
+                <li>
+                    <a href="https://www.youtube.com/user/HIDETCHI">HIDETCHI</a> : A lot of great videos in English.
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/channel/UCRnXG7CkKfEN6IINKcO_uBg">Shogi Harbour</a> : A newer and active English shogi channel with tutorials and game commentaries.
+                </li>
             </ul>
         </div>
     );
